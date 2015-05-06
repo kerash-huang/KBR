9403c7324898432a89f5e8d8cfd5788c1c06f034
Kerash <kerash@livemail.tw>
2015-05-07 01:58:16 +0800

    Modify encrypt method - DES CBC MODE

--- security/Encrypt.php
+++ security/Encrypt.php
@@ -2,39 +2,20 @@
 namespace security;
 class Encrypt {
 
-    public function DES_CBC($text, $key, $iv, $base64 = true) {
-        
-        $td        = mcrypt_module_open(MCRYPT_DES, '', MCRYPT_MODE_CBC, '');
-        $KeySize   = mcrypt_get_key_size(MCRYPT_DES,MCRYPT_MODE_CBC);
-        $BlockSize = mcrypt_get_block_size(MCRYPT_DES,MCRYPT_MODE_CBC);
-
-        $str = self::pkcs5padding($str, $BlockSize);
-
-        $hashkey = sha1($key);
-
-        mcrypt_generic_init($td, substr($hashkey, 0, $KeySize) , $iv);
-
-        $enc = mcrypt_generic($td, $str);
-        mcrypt_generic_deinit($td);
-        mcrypt_module_close($td);
+    public static function DES_CBC($string, $key, $iv = "", $base64 = true) {
+        $blockSize = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_CBC);
+        $string = self::pkcs5padding($string, $blockSize);
+        $encoded = mcrypt_encrypt(MCRYPT_DES, $key, $string, MCRYPT_MODE_CBC, $iv);
         if($base64) {
-            $enc = base64_encode($enc);
+            $encoded = base64_encode($encoded);
         }
-        return $enc;
+        return $encoded;
     }
 
-
-    private function pkcs5padding($str, $blocksize) {
-        $padsize = $blocksize - (strlen($str) % $blocksize);
-        $str .= str_repeat(chr($padsize), $padsize);
-        return $str;
+    public static function pkcs5padding($text, $blocksize)
+    {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
     }
-
-
-
-
-
-
-
-
+    
 }
