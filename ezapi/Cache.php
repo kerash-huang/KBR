b6be1f8cfa681dc80d15faefbf0e9faea1d621eb
Kerash <kerash@livemail.tw>
2015-06-29 21:08:34 +0800

    fixed

--- ezapi/Cache.php
+++ ezapi/Cache.php
@@ -3,13 +3,11 @@ namespace ezapi;
 
 class Cache {
 
-    
-    
+    private $base_cache_path = __DIR__."/cache/";   
 
     public static function SetFileCache( $filename, $content ) {
 
-
-
+        return file_put_contents($base_cache_path.$filename, $content);
 
     }
 
